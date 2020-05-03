c2ac9fa294a5b7a1b8de20678afb370e2200fca3
jameskokoska <jameskokoska@users.noreply.github.com>
2020-05-02 21:13:13 -0400

    fixed compilation errors with stateful

--- animal_crossing_app/lib/fish.dart
+++ animal_crossing_app/lib/fish.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
 
-//class FishPage extends StatelessWidget{
-//  @override
-//  Widget build(BuildContext context){
-//    return Container(
-//      height: MediaQuery.of(context).size.height,
-//      color: Colors.blue,
-//    );
-//  }
-//}
-
 class Fish extends StatefulWidget {
   Fish({Key key, this.title}) : super(key: key);
 
@@ -17,4 +7,14 @@ class Fish extends StatefulWidget {
 
   @override
   _FishPageState createState() => _FishPageState();
+}
+
+class _FishPageState extends State<Fish>{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      color: Colors.blue,
+    );
+  }
 }
