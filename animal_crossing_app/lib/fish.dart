8d1a7e272f39cc4282575c5ff9107657f709c6ef
randsco <ryan1wong@gmail.com>
2020-05-02 20:15:51 -0400

    Added .idea files. Converted fish page to stateful widget. pubspec.lock updated.

--- animal_crossing_app/lib/fish.dart
+++ animal_crossing_app/lib/fish.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-class FishPage extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      color: Colors.blue,
-    );
-  }
+//class FishPage extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context){
+//    return Container(
+//      height: MediaQuery.of(context).size.height,
+//      color: Colors.blue,
+//    );
+//  }
+//}
+
+class Fish extends StatefulWidget {
+  Fish({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _FishPageState createState() => _FishPageState();
 }
